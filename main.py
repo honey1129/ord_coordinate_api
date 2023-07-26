c617a9cbcb690cc12eb0fbfdda9127e9a3cf796d
Rentero <weixiurenyuanyong@MacBook-Pro-2.local>
2023-07-26 14:53:39 +0800

    f

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ async def get_valid_coordinates(request: Request,
     all_coordinates_data_list = crud.get_all_coordinates_info_no_limit(db=db)
     for i in all_coordinates_data_list['data']:
         try:
-            a = i['Coordinates'][1:-1].split(',')
+            a = i['Coordinates'].strip()[1:-1].split(',')
             i['Coordinates'] = str(tuple([int(x) for x in a]))
         except:
             pass
